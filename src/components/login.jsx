78dab928be44af2299d7148a8749d72555ee3c54
Praxash2K <prakashpaini125@gmail.com git config --global user.email prakashpaini125@gmail.com>
2024-06-29 13:19:22 +0530

    added alert for users

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -4,28 +4,24 @@ import styled from 'styled-components'
 
 export default function Login() {
     const handleClick = () => {
+        alert('The user needs to be added to userbase for this to work properly this is due to spotify developer policy you may contact me @ prakashpaini125@gmail.com');
         const clientId = 'e4717cbd56e4478695d623da227597f2';
         const redirectUrl = window.location.href.split('?')[0];
         const apiurl = 'https://accounts.spotify.com/authorize';
         const scopes = [
-            'user-read-email',
-            'user-read-private',
             'user-modify-playback-state',
             'user-read-playback-state',
             'user-read-currently-playing',
             'user-read-recently-played',
             'user-follow-read',
+            'user-library-read',
+            'user-read-email',
+            'user-read-private',
             'user-top-read',
             'playlist-read-private',
             'playlist-read-collaborative',
-            'streaming',
-            'user-library-read',
             'user-read-playback-position',
-            'user-read-playback-state',
-            'user-read-currently-playing',
-            'user-read-recently-played',
-            'user-follow-read',
-            'user-top-read',
+            'streaming'
         ]
 
         const url = `${apiurl}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
